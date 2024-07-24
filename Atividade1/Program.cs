90a653d45b71df9fb6026dbfcaac0e0ff86b91f6
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-07-24 10:14:13 -0300

    Finalizar atividade

--- Atividade1/Program.cs
+++ Atividade1/Program.cs
@@ -10,7 +10,66 @@ class HelloWorld
 
 class Calculadora
 {
+    public static void Inicia()
+    {
+        Console.WriteLine("Selecione a Operação:\n" +
+            "1) Soma\n" +
+            "2) Subtração\n" +
+            "3) Multiplicação\n" +
+            "4) Divisão");
 
+        Console.Write(">");
+        string op = Console.ReadLine();
+
+        Console.Write("Numero 1:");
+        float num1 = float.Parse(Console.ReadLine());
+        Console.Write("Numero 2:");
+        float num2 = float.Parse(Console.ReadLine());
+
+        float? resultado = Calcula(op, num1, num2);
+
+        if (resultado == null)
+        {
+            Console.WriteLine("Operação Inválida");
+            return;
+        }
+
+        Console.WriteLine($"Resultado: {resultado}");
+    }
+
+    public static float? Calcula(string op, float num1, float num2)
+    {
+        switch(op)
+        {
+            case "1":
+                return num1 + num2;
+            case "2":
+                return num1 - num2;
+            case "3":
+                return num1 * num2;
+            case "4":
+                return num1 / num2;
+            default:
+                return null;
+        }
+    }
+}
+
+class ParImpar
+{
+    public static void Inicia()
+    {
+        Console.Write("Digite um Número: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        if (num % 2 == 0)
+        {
+            Console.WriteLine("Par!");
+        } else
+        {
+            Console.WriteLine("Ímpar");
+        }
+    }
 }
 
 class Program
@@ -27,6 +86,7 @@ class Program
                 "3) Par ou Ímpar\n" +
                 "4) Sair");
 
+            Console.Write(">");
             string escolha = Console.ReadLine();
 
             switch (escolha)
@@ -35,10 +95,11 @@ class Program
                     HelloWorld.Escreve();
                     break;
                 case "2":
-
+                    Calculadora.Inicia();
                     break;
                 case "3":
-
+                    ParImpar.Inicia();
+                    break;
                 case "4":
                     deve_sair = true;
                     break;
